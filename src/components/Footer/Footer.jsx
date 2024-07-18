c72697356c167f63f9e56e3a5e4d19167c96bd1c
harith01 <harithyusuph@gmail.com>
2024-07-18 11:45:17 +0100

    Create Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>CryptoApp 2024</p>
+        </div>
+    )
+}
+
+export default Footer;
