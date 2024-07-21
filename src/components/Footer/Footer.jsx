f3bf8173b7b33566e89b2fa100c579ba661a828d
harith01 <harithyusuph@gmail.com>
2024-07-21 14:19:50 +0100

    Make the year text in the Footer to be dynamic

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            <p>CryptoApp 2024</p>
+            <p>CryptoApp {new Date().getFullYear()}</p>
         </div>
     )
 }
